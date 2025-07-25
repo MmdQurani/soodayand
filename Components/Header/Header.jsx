6b6f2ee3c4f7e64f7d7b55db6df2062acd1f61d5
78 mmd 69 <mmdqurani@gmail.com>
2025-07-25 03:34:16 +0330

    Header Responsiveness for All Sizes

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -10,42 +10,65 @@ import Date_Day from '../Date_Day/Date_Day'
 function Header() {
   return (
     <header className='w-auto h-12 top-4 sticky'>
-      <div className='w-auto h-full grid grid-cols-12 gap-x-3 text-gray-500'>
-        
-        {/* نام صفحه */}
-        <div className='xl:col-span-5 col-span-3 bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center px-4'>
-          <SlHome className='inline-block text-xl' />
-          <span className='text-md font-bold mr-2'>داشبورد</span>
-        </div>
+      {/* محتوای هدر برای اندازه های بزرگتر از تبلت */}
+      <div className='w-full h-full lg:flex hidden'>
 
-        {/* نام کاربر */}
-        <div className='col-span-3 bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center px-4'>
-          <HiOutlineUserCircle className='inline-block text-xl' />
-          <Link to='/' className='text-sm font-bold mr-1'>مرتضی آقاپور</Link>
-        </div>
+        <div className='w-full h-full grid grid-cols-12 gap-x-3 text-gray-500'>
 
-        {/* تاریخ شمسی */}
-        <div className='xl:col-span-3 col-span-4 cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-between px-4'>
-          <div>
-            <HiOutlineUserCircle className='inline-block text-xl' />
-            <span className='text-sm font-bold mr-1'>تاریخ روز :</span>
+          {/* نام صفحه */}
+          <div className='xl:col-span-5 col-span-3 bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center px-4'>
+            <SlHome className='inline-block text-xl' />
+            <span className='text-md font-bold mr-2'>داشبورد</span>
           </div>
 
-          {/* اینجا کامپوننت تاریخ شمسی  */}
-          <Date_Day />
+          {/* نام کاربر */}
+          <div className='col-span-3 bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center px-4'>
+            <HiOutlineUserCircle className='inline-block text-xl' />
+            <Link to='/' className='text-sm font-bold mr-1'>مرتضی آقاپور</Link>
+          </div>
+
+          {/* تاریخ شمسی */}
+          <div className='xl:col-span-3 col-span-4 cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-between px-4'>
+            <div>
+              <HiOutlineUserCircle className='inline-block text-xl' />
+              <span className='text-sm font-bold mr-1'>تاریخ روز :</span>
+            </div>
+
+            {/* اینجا کامپوننت تاریخ شمسی  */}
+            <Date_Day />
+          </div>
+
+          {/* آیکون‌های اعلان و خروج */}
+          <div className='xl:col-span-1 col-span-2 h-full flex flex-row justify-around items-center gap-x-2'>
+            <button className='cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-center w-full h-full'>
+              <IoMdNotificationsOutline className='inline-block text-2xl text-gray-500' />
+            </button>
+
+            <button className='cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-center w-full h-full'>
+              <HiArrowLeftStartOnRectangle className='inline-block text-2xl text-gray-500' />
+            </button>
+          </div>
+
         </div>
 
-        {/* آیکون‌های اعلان و خروج */}
-        <div className='xl:col-span-1 col-span-2 h-full flex flex-row justify-around items-center gap-x-2'>
-          <button className='cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-center w-full h-full'>
+      </div>
+
+      {/* محتوای هدر برای اندازه های تبلت و کوچکتر از تبلت */}
+      <div className='w-full h-full lg:hidden flex flex-row justify-between items-center bg-white rounded-md px-4'>
+        <div className='max-w-[100px] h-full flex items-center justify-center gap-x-2'>
+          <img src="../../public/images/Logo/AyandehLogo-CbE1UbbU.svg" className='w-full object-contain' alt="لوگو" />
+        </div>
+
+        <div className='max-w-max h-max px-4 py-2 rounded-md flex items-center justify-center gap-x-2 bg-gray-100'>
+          {/* <HiOutlineUserCircle className='inline-block text-2xl' /> */}
+          <Link to='/' className='text-sm font-bold'>مرتضی آقاپور</Link>
+        </div>
+
+        <div className='max-w-max h-full flex items-center justify-center gap-x-2'>
+          <button className='cursor-pointer hover:text-gray-800 rounded-full flex items-center justify-center w-8 h-8 bg-gray-100 hover:shadow-md transition-all duration-100'>
             <IoMdNotificationsOutline className='inline-block text-2xl text-gray-500' />
           </button>
-
-          <button className='cursor-pointer bg-white hover:text-gray-800 hover:shadow-md transition-all duration-100 rounded-md flex items-center justify-center w-full h-full'>
-            <HiArrowLeftStartOnRectangle className='inline-block text-2xl text-gray-500' />
-          </button>
         </div>
-
       </div>
     </header>
   )
