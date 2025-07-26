a49b651de565fdedfeca1956264d93a97e6ecb9b
78 mmd 69 <mmdqurani@gmail.com>
2025-07-26 10:14:11 +0330

    Display data in Persian format

--- Components/Lable_User_Wealth/Lable_User_Wealth.jsx
+++ Components/Lable_User_Wealth/Lable_User_Wealth.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Lable_User_Wealth({ wealthText, wealthAmount = "0", first = false }) {
+function Lable_User_Wealth({ wealthText, wealthAmount = 0, first = false }) {
   return (
     <div className='flex flex-row justify-between items-center font-normal text-gray-700 text-sm py-4'>
       <span>{wealthText}</span>
       <span className={`${first ? 'text-green-600' : 'text-gray-800'}`}>
-        {wealthAmount} (ریال)
+        {wealthAmount.toLocaleString('fa-IR')} (ریال)
       </span>
     </div>
   )
